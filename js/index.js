542744f11ce576296b0a2c56735b6edc84a9acdc
Valentin <vmihaila333@gmail.com>
2023-03-08 10:27:30 +0200

    now you can post

--- js/index.js
+++ js/index.js
@@ -1,16 +1,20 @@
+let postsArray = []
+
+function renderPosts () {
+    let html = ''
+    for(let post of postsArray){
+    html += `<h1> ${post.title} </h1>
+                <p> ${post.body} <p>
+                <div class="bar"></div>
+    `
+    document.querySelector('#content').innerHTML = html
+}}
+
 fetch('https://apis.scrimba.com/jsonplaceholder/posts')
     .then(response => response.json())
     .then(data => {
-        const postsArray = data.slice(0, 5)
-        let html = ""
-        for(let post of postsArray){
-            html += `
-            <h1> ${post.title} </h1>
-            <p> ${post.body} <p>
-            <div class="bar"></div>
-            `
-        }
-        document.querySelector('#content').innerHTML = html
+        postsArray = data.slice(0, 5)
+        renderPosts()
     }) 
 
 
@@ -23,6 +27,21 @@ document.getElementById('post').addEventListener('submit', (e) => {
         title: title,
         body:postBody
     }
+
+    const options = {method: 'POST',
+                    body: JSON.stringify(data),
+                    headers: {
+                            'Content-Type': 'application/json'
+    }}
+
+    fetch('https://apis.scrimba.com/jsonplaceholder/posts', options)
+        .then(res => res.json())
+        .then(post => {
+            postsArray.unshift(post)
+            renderPosts()
+        })
+
+
     document.getElementById('post-body').value = ''
     document.getElementById('title').value = ''
 })
